61b72013939f322ec09ad803a2ee399129297e0d
Baratov Viktor <vitya.baratov@gmail.com>
2024-10-20 14:34:11 +0300

    add open and close functions for popup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './pages/index.css'; 
 import { displayCards } from './scripts/components/cards';
+import { openModal, closeModal } from './scripts/components/modal';
+
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const popupImage = document.querySelector('.popup_type_image');
+
+const profileEditButtonOpen = document.querySelector('.profile__edit-button');
+const profileAddButtonOpen = document.querySelector('.profile__add-button');
+const profileEditButtonClose = popupEdit.querySelector('.popup__close');
+const profileAddButtonClose = popupNewCard.querySelector('.popup__close');
+
 displayCards();
+
+profileEditButtonOpen.addEventListener('click', function(){
+    openModal(popupEdit);
+});
+profileAddButtonOpen.addEventListener('click', function(){
+    openModal(popupNewCard);
+});
+profileEditButtonClose.addEventListener('click', function(){
+    closeModal(popupEdit);
+});
+profileAddButtonClose.addEventListener('click', function(){
+    closeModal(popupNewCard);
+});
+
+
---
+++ src/scripts/components/modal.js
@@ -0,0 +1,8 @@
+
+export function openModal(modalWindow) {
+    modalWindow.classList.add('popup_is-opened');
+}
+export function closeModal(modalWindow) {
+    modalWindow.classList.remove('popup_is-opened');
+}
+
